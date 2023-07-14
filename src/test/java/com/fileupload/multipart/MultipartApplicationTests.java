35079b5a4b1c38e4379c2a4970ce455d7f78a0a6
dickonkudzai <dickonkudzai@live.com>
2023-07-14 07:56:01 +0200

    initial commit

---
+++ src/test/java/com/fileupload/multipart/MultipartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileupload.multipart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
